b97d06a6435b1895c6244816ae4d97281b729c05
crazy coder <rajanjasani290@gmail.com>
2025-12-02 23:41:56 +0530

    Update day2.java

--- day2.java
+++ day2.java
@@ -15,3 +15,20 @@ class index{
   }
 }
 
+// used for input floating number
+import java.util.*;
+class index{
+  public static void main(String arg[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print(" Enter numbers n1: ");
+    float n1 = sc.nextFloat();
+
+    System.out.print(" Enter numbers n2: ");
+    float n2 = sc.nextFloat();
+    float sum = n1 + n2;
+    System.out.println(" Sum: "+sum);
+    
+  }
+}

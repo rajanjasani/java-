2af78aab5e4696c15874c4f342e9ca327d79b896
crazy coder <rajanjasani290@gmail.com>
2025-12-02 23:40:21 +0530

    Update day2.java

--- day2.java
+++ day2.java
@@ -1,1 +1,17 @@
 
+import java.util.*;
+class index{
+  public static void main(String arg[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print(" Enter numbers n1: ");
+    int n1 = sc.nextInt();
+    
+    System.out.print(" Enter numbers n2: ");
+    int n2 = sc.nextInt();
+    int sum = n1 + n2;
+    System.out.println(" Sum: "+sum);
+  }
+}
+
